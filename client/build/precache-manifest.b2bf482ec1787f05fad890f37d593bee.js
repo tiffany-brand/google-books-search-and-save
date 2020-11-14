f5c5ad3a093881d036443cf1f719ab8618b96689
Tiffany Brand <brand2learn@gmail.com>
2020-11-13 21:03:03 -0500

    Build client

---
+++ client/build/precache-manifest.b2bf482ec1787f05fad890f37d593bee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89f4560d3068ef34458abdca08388f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "523b6785b7c99fdd4bab",
+    "url": "/static/css/main.6620a48d.chunk.css"
+  },
+  {
+    "revision": "4122841d37d3b5e8c183",
+    "url": "/static/js/2.de1369e0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.de1369e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523b6785b7c99fdd4bab",
+    "url": "/static/js/main.d4c7ccf4.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "2e6f16af9f7a4953104a0c9bdf1262e8",
+    "url": "/static/media/books-bg.2e6f16af.jpg"
+  }
+]);
